5a6928f7c10e1057364623fc38e5397c6268f455
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2021-11-20 16:51:14 +0100

    Initializing actionSHARK project

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup, find_packages
+
+if not sys.version_info[0] == 3:
+    print('only python3 supported!')
+    sys.exit(1)
+
+setup(
+    name='actionSHARK',
+    version='0.0.1',
+    author='',
+    author_email='',
+    description='Collect data from actions',
+    install_requires=['mongoengine', 'pymongo', 'requests>=2.10.0', 'oauthlib>=3.0.0','pycoshark>=1.3.2'],
+    url='https://github.com/smartshark/actionSHARK',
+    download_url='https://github.com/smartshark/actionSHARK/zipball/master',
+    packages=find_packages(),
+    test_suite='tests',
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache2.0 License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
