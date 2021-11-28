675a6194cf161f185570fb1be0d09f99652208a8
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2021-11-28 19:49:09 +0100

    implementing paginating in get_runs()

--- actionshark/github.py
+++ actionshark/github.py
@@ -1,16 +1,26 @@
 import json
-import requests
-from requests.auth import HTTPBasicAuth
 from typing import Optional
 
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+'''
+Notes:
+API requests using Basic Authentication or OAuth, you can make up to 5,000 requests per hour
+checking limit: https://api.github.com/users/hatahetahmad
+Secondary rate limits are not intended to interfere with legitimate use of the API.
+Your normal rate limits should be the only limit you target.
+
+'''
 
 class GitHub():
     """
     Managing different type of get Request to fetch data from GitHub REST API"""
 
-    base_url = 'https://api.github.com/'
+    __base_url = 'https://api.github.com/'
 
-    search_types = {
+    __search_dict = {
         'issues' : 'search/issues',
         'code' : 'search/code',
         'repositories' : 'search/repositories',
@@ -26,14 +36,19 @@ class GitHub():
         with open(file_path, 'r', encoding='utf-8') as f:
             lines = json.load(f)
         self.token = lines.get('access_token')
-        self.headers = {
+        self.__headers = {
             'Accept': 'application/vnd.github.v3+json',
             'Authentication': f'token {self.token}'
         }
 
 
+    @property
+    def search_types(self):
+        return [key for key in self.__search_dict.keys()]
 
-    def save_JSON(self, save_path: str, response: requests.models.Response, verbose: bool = False) -> str:
+
+
+    def save_JSON(self, save_path: str, response: requests.models.Response, verbose: bool = False) -> bool:
         """Saving a JSON response from GitHub API after checking response status.
 
         Args:
@@ -43,14 +58,23 @@ class GitHub():
         """
         if response.status_code == 200:
             if verbose: print('Successful Request!')
+
+            response_JSON = response.json()
+            if not response_JSON.get('workflow_runs'): return False
+
             with open( save_path, 'w', encoding='utf-8') as f:
-                json.dump(response.json(), f, indent=4)
+                json.dump(response_JSON, f, indent=4)
             if verbose: print(f'Response is saved in: {save_path}')
             if verbose: print('-'*60)
+
+            return True
+
         else:
             print(f'ERROR: {response.status_code}')
             if verbose: print(f'ERROR: {response.reason}')
             print('-'*60)
+
+            return False
 
 
 
@@ -67,11 +91,6 @@ class GitHub():
 
 
 
-    def get_search_types(self) -> list[str]:
-        return [key for key in self.__search_types.keys()]
-
-
-
     def get_search(self, search_type: str, q: dict = None, save_path: Optional[str] = None, verbose: bool = False) -> None:
         """Fetching search data from GitHub API for types like issues, user, ... etc.
 
@@ -81,8 +100,8 @@ class GitHub():
             save_path (Optional[str], optional): File name and path to where the response should be saved. Defaults to "./data/search/search_{search_type}.json".
             verbose (bool): Print extra information to console.
         """
-        if search_type in self.__search_types:
-            url = self.base_url + self.__search_types[search_type]
+        if search_type in self.__search_dict:
+            url = self.__base_url + self.__search_dict[search_type]
         else:
             print('Unrecognized / Unsupported search criteria!')
 
@@ -92,7 +111,7 @@ class GitHub():
         else:
             github_url = url
 
-        response = requests.request('GET', github_url, headers=self.headers)
+        response = requests.request('GET', github_url, headers=self.__headers)
 
         if verbose: print('GitHub API URL:', github_url)
 
@@ -115,9 +134,9 @@ class GitHub():
         if not owner or not repo: raise 'Please make to sure to pass both the owner and repo names.'
 
         url = 'repos/{owner}/{repo}/actions/workflows'
-        github_url = self.base_url + url.format(owner=owner, repo=repo)
+        github_url = self.__base_url + url.format(owner=owner, repo=repo)
 
-        response = requests.request('GET', github_url, headers=self.headers)
+        response = requests.request('GET', github_url, headers=self.__headers)
 
         if verbose: print('GitHub API URL:', github_url)
 
@@ -140,9 +159,9 @@ class GitHub():
 
         # url = 'orgs/{org}/actions/permissions/repositories'
         url = 'orgs/{org}/repos'
-        github_url = self.base_url + url.format(org=organization)
+        github_url = self.__base_url + url.format(org=organization)
 
-        response = requests.request('GET', github_url, headers=self.headers)
+        response = requests.request('GET', github_url, headers=self.__headers)
 
         if verbose: print('GitHub API URL:', github_url)
 
@@ -153,7 +172,7 @@ class GitHub():
 
 
 
-    def get_runs(self, owner: Optional[str] = None, repo: Optional[str] = None, per_page: int = 100, page: int = 1, created = None, exclude_pull_requests: bool = False, save_path: Optional[str] = None, verbose: bool = False) -> None:
+    def get_runs(self, owner: Optional[str] = None, repo: Optional[str] = None, per_page: int = 100, page: int = 1, created = None, exclude_pull_requests: bool = True, save_path: Optional[str] = None, verbose: bool = False) -> None:
         """Fetching workflow runs data from GitHub API for specific repository and owner.
 
         Args:
@@ -167,21 +186,29 @@ class GitHub():
 
         if not owner or not repo: raise 'Please make to sure to pass both the owner and repo names.'
 
-        q = {'per_page': per_page, 'page': page, 'created': created, 'exclude_pull_requests': exclude_pull_requests}
+        q = {'per_page': str(per_page), 'created': created, 'exclude_pull_requests': str(exclude_pull_requests) }
         if not created: del q['created']
 
         url = 'repos/{owner}/{repo}/actions/runs'
-        github_url = self.base_url + url.format(owner=owner, repo=repo) + f'?{self.parameter_constructor(q)}'
+        github_url = self.__base_url + url.format(owner=owner, repo=repo) + f'?{self.parameter_constructor(q)}'
 
-        response = requests.request('GET', github_url, headers=self.headers)
+        next_page = 0
+        while True:
 
-        if verbose: print('GitHub API URL:', github_url)
+            next_page += page
+            github_url += f'&page={next_page}'
 
-        if not save_path:
-            save_path = f'./data/runs/{owner}_{repo}_runs.json'
+            response = requests.request('GET', github_url, headers=self.__headers)
 
-        self.save_JSON(save_path, response)
+            if verbose: print('GitHub API URL:', github_url)
 
+            if not save_path:
+                if not created: save_path = f'./data/runs/{owner}_{repo}_runs_{next_page}.json'
+                else: save_path = f'./data/runs/{owner}_{repo}_runs_{created}_{next_page}.json'
+
+            if not self.save_JSON(save_path, response): break
+
+            github_url = github_url[:-len(f'&page={next_page}')]
 
 
     def get_runs_artifacts(self, owner: Optional[str] = None, repo: Optional[str] = None, run_id: int = None, save_path: Optional[str] = None, verbose: bool = False) -> None:
@@ -197,9 +224,9 @@ class GitHub():
 
         if not owner or not repo or not run_id: raise 'Please make to sure to pass both the owner and repo names.'
         url = 'repos/{owner}/{repo}/actions/runs/{run_id}/artifacts'
-        github_url = self.base_url + url.format(owner=owner, repo=repo, run_id=run_id)
+        github_url = self.__base_url + url.format(owner=owner, repo=repo, run_id=run_id)
 
-        response = requests.request('GET', github_url, headers=self.headers)
+        response = requests.request('GET', github_url, headers=self.__headers)
 
         if verbose: print('GitHub API URL:', github_url)
 
@@ -211,19 +238,19 @@ class GitHub():
 if __name__ == '__main__':
 
 
-    cls = GitHub('settings.json')
+    cls_GitHub = GitHub('settings.json')
 
     # q = {
     #     'language' : 'python',
     #     'star' : '>50',
     #     'label' : 'open'
     # }
-    # cls.get_search('issues', q)
+    # cls_GitHub.get_search('issues', q)
 
 
     # Get all organization
     # q = {'type':'org'}
-    # cls.get_search('users', q)
+    # cls_GitHub.get_search('users', q)
 
     # owner_name = 'smartshark'
     # repo_name = 'issueSHARK'
@@ -231,14 +258,14 @@ if __name__ == '__main__':
     # owner_name = 'fireship-io'
     # # repo_name = 'fireship' # neither artifacts or workflows
     # repo_name = '225-github-actions-demo' # only workflows
-    # cls.get_workflow(owner_name, repo_name)
-    # cls.get_runs(owner_name, repo_name)
+    # cls_GitHub.get_workflow(owner_name, repo_name)
+    # cls_GitHub.get_runs(owner_name, repo_name)
 
     # org_name = 'fireship-io'
-    # cls.get_organization_repostries(org_name)
+    # cls_GitHub.get_organization_repostries(org_name)
 
     # org_name = 'smartshark'
-    # cls.get_organization_repostries(org_name)
+    # cls_GitHub.get_organization_repostries(org_name)
 
     # get all artifacts for runs
     # with open ('./data/fireship-io_225-github-actions-demo_action_runs.json', 'r', encoding='utf-8') as f:
@@ -248,6 +275,12 @@ if __name__ == '__main__':
     # for run in runs['workflow_runs']:
     #     # print(run['repository']['owner']['login'],run['repository']['name'],type(run['id']) )
     #     count_runs += 1
-    #     cls.get_runs_artifacts(run['repository']['owner']['login'],run['repository']['name'],run['id'])
+    #     cls_GitHub.get_runs_artifacts(run['repository']['owner']['login'],run['repository']['name'],run['id'])
     #     # break
     # print('count_runs:', count_runs)
+
+
+    owner_name = 'freeCodeCamp'
+    repo_name = 'freeCodeCamp'
+    cls_GitHub.get_runs(owner_name, repo_name, verbose=True)
+    # cls_GitHub.get_runs(owner_name, repo_name, created='2021-11-28')
