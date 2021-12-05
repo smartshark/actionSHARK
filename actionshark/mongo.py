72ee02afff7784cfcbbed6ab23261d41b73afc6e
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2021-12-05 22:51:34 +0100

    change class names and use pycoShark for all imports

--- actionshark/mongo.py
+++ actionshark/mongo.py
@@ -1,15 +1,13 @@
-from pycoshark.utils import *
-from pymongo import MongoClient
-from mongoengine import Document, StringField, IntField, ObjectIdField, DateTimeField, ListField, BooleanField, DictField
+import pycoshark.utils as utils
 
 # https://cloud.mongodb.com/v2/6182ecb3f5d77671953f0a32#clusters
 # mongodb+srv://<username>:<password>@cluster0.3dpvx.mongodb.net/DB_name
 
 
-class GitHub_Repository(Document):
-    id = IntField(unique=True)
-    name = StringField()
-    description =  StringField()
+class Repositories(utils.Document):
+    id = utils.IntField(unique=True)
+    name = utils.StringField()
+    description =  utils.StringField()
     # "owner": { # do users/organization instead of for owners and contributor
     #     "login": "freeCodeCamp",
     #     "id": 9892522,
@@ -19,70 +17,70 @@ class GitHub_Repository(Document):
 
 
 
-class GitHub_Workflow(Document):
-    id = IntField(unique=True)
-    name = StringField()
-    path = StringField()
-    state = StringField()
-    created_at = DateTimeField(default=None)
-    updated_at = DateTimeField(default=None)
+class Workflows(utils.Document):
+    id = utils.IntField(unique=True)
+    name = utils.StringField()
+    path = utils.StringField()
+    state = utils.StringField()
+    created_at = utils.DateTimeField(default=None)
+    updated_at = utils.DateTimeField(default=None)
     pass
 
 
 
-class GitHub_Run(Document):
-    id = IntField(unique=True)
-    name = StringField()
-    head_branch = StringField()
-    run_number = IntField()
-    event = StringField(max_length=15, required=True)
-    status = StringField()
-    conclusion = StringField()
-    workflow_id = ObjectIdField()
-    pull_requests =  DateTimeField(default=None)
-    created_at = DateTimeField(default=None)
-    updated_at = DateTimeField(default=None)
-    run_attempt = IntField()
-    run_started_at = DateTimeField(default=None)
-    head_commit =  ObjectIdField()
+class Runs(utils.Document):
+    id = utils.IntField(unique=True)
+    name = utils.StringField()
+    head_branch = utils.StringField()
+    run_number = utils.IntField()
+    event = utils.StringField(max_length=15, required=True)
+    status = utils.StringField()
+    conclusion = utils.StringField()
+    workflow_id = utils.ObjectIdField()
+    pull_requests =  utils.DateTimeField(default=None)
+    created_at = utils.DateTimeField(default=None)
+    updated_at = utils.DateTimeField(default=None)
+    run_attempt = utils.IntField()
+    run_started_at = utils.DateTimeField(default=None)
+    head_commit =  utils.ObjectIdField()
     pass
 
 
 
-class GitHub_Job(Document):
-    id = IntField(unique=True)
-    name = StringField()
-    run_number = ObjectIdField()
-    run_attempt = IntField()
-    status = StringField()
-    conclusion = StringField()
-    started_at = DateTimeField(default=None)
-    completed_at = DateTimeField(default=None)
-    steps = ListField( DictField() )
-    runner_id = IntField()
-    runner_name = StringField()
-    runner_group_id = IntField()
-    runner_group_name = StringField()
+class Jobs(utils.Document):
+    id = utils.IntField(unique=True)
+    name = utils.StringField()
+    run_number = utils.ObjectIdField()
+    run_attempt = utils.IntField()
+    status = utils.StringField()
+    conclusion = utils.StringField()
+    started_at = utils.DateTimeField(default=None)
+    completed_at = utils.DateTimeField(default=None)
+    steps = utils.ListField( utils.DictField() )
+    runner_id = utils.IntField()
+    runner_name = utils.StringField()
+    runner_group_id = utils.IntField()
+    runner_group_name = utils.StringField()
     pass
 
 
 
-class GitHub_Artifacts(Document):
-    id = IntField(unique=True)
-    name = StringField()
-    size_in_bytes = IntField()
-    archive_download_url = StringField()
-    expired = BooleanField()
-    created_at = DateTimeField(default=None)
-    updated_at = DateTimeField(default=None)
-    expires_at = DateTimeField(default=None)
+class Artifacts(utils.Document):
+    id = utils.IntField(unique=True)
+    name = utils.StringField()
+    size_in_bytes = utils.IntField()
+    archive_download_url = utils.StringField()
+    expired = utils.BooleanField()
+    created_at = utils.DateTimeField(default=None)
+    updated_at = utils.DateTimeField(default=None)
+    expires_at = utils.DateTimeField(default=None)
     pass
 
 
 
 class Mongo:
-    __conn_uri = create_mongodb_uri_string(db_user=None, db_password=None, db_hostname='localhost', db_port=27017, db_authentication_database=None, db_ssl_enabled=False)
-    __conn = MongoClient(__conn_uri)
+    __conn_uri = utils.create_mongodb_uri_string(db_user=None, db_password=None, db_hostname='localhost', db_port=27017, db_authentication_database=None, db_ssl_enabled=False)
+    __conn = utils.MongoClient(__conn_uri)
 
     def is_connected(self):
         return print(self.__conn)
