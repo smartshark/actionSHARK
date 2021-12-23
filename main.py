02c81fb4957c56f95c7471749ec26380f235908b
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2021-12-23 22:19:53 +0100

    parse owner and repo name from url

--- main.py
+++ main.py
@@ -10,50 +10,97 @@ from actionshark.github import GitHub
 
 # parsing command line arguments
 def collect_args():
-    parser = utils.get_base_argparser('Collects information from command line.', '0.0.1')
+    parser = utils.get_base_argparser(
+        "Collects information from command line.", "0.0.1"
+    )
 
     # GitHub Args
-    parser.add_argument('-t', '--token', help='GitHub token to authenticate user.', required=False, type=str)
-    parser.add_argument('-env', '--token-env', help='Environment variable, where token is stored.', required=False, type=str)
-    parser.add_argument('-o', '--owner', help='Owner name of the repository.', required=True, type=str)
-    parser.add_argument('-r', '--repository', help='Repository Name.', required=True, type=str)
+    parser.add_argument(
+        "-t",
+        "--token",
+        help="GitHub token to authenticate user.",
+        required=False,
+        type=str,
+    )
+    parser.add_argument(
+        "-env",
+        "--token-env",
+        help="Environment variable, where token is stored.",
+        required=False,
+        type=str,
+    )
+    parser.add_argument(
+        "-o",
+        "--owner",
+        help="Owner name of the repository.",
+        default=None,
+        required=False,
+        type=str,
+    )
+    parser.add_argument(
+        "-r",
+        "--repository",
+        help="Repository Name.",
+        default=None,
+        required=False,
+        type=str,
+    )
+    parser.add_argument(
+        "-ru",
+        "--url",
+        help="Repository URL.",
+        default=None,
+        required=False,
+        type=str,
+    )
 
     # General
-    parser.add_argument('--debug', help='Sets the debug level.', default='DEBUG',
-                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'])
+    parser.add_argument(
+        "--debug",
+        help="Sets the debug level.",
+        default="DEBUG",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+    )
 
     return parser.parse_args()
 
 
 # load logger configuration
-with open('./logger_config.json', 'r') as f:
-        logging.config.dictConfig( json.load(f) )
+with open("./logger_config.json", "r") as f:
+    logging.config.dictConfig(json.load(f))
 
 
 # main function
 def main():
     # Start logger
-    logger = logging.getLogger('main')
-    logger.debug('Start Logging')
+    logger = logging.getLogger("main")
+    logger.debug("Start Logging")
 
     # collect args from terminal to cfg variable
     args = collect_args()
     cfg = Config(args)
 
     # initiate mongo instance
-    mongo = Mongo(cfg.db_user, cfg.db_password, cfg.db_hostname, cfg.db_port, cfg.db_database, cfg.db_authentication, cfg.db_ssl)
-
-    # *DEBUGGING
-    mongo.drop_database()
+    mongo = Mongo(
+        cfg.db_user,
+        cfg.db_password,
+        cfg.db_hostname,
+        cfg.db_port,
+        cfg.db_database,
+        cfg.db_authentication,
+        cfg.db_ssl,
+    )
 
     # initiate GitHub instance
-    github = GitHub(owner=cfg.owner, repo=cfg.repo, token=cfg.token, save_mongo=mongo.save_documents)
+    github = GitHub(
+        owner=cfg.owner, repo=cfg.repo, token=cfg.token, save_mongo=mongo.save_documents
+    )
 
     # get all actions
-    github.run( mongo.runs )
+    github.run(mongo.runs)
 
     # Finish logging message
-    logger.debug('Finish Logging')
+    logger.debug("Finish Logging")
 
 
 if __name__ == "__main__":
