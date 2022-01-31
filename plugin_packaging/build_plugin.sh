0a8045d388b930372b1c309c80047c7e126afa09
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2022-01-31 07:33:58 +0100

    build tar plugin from within plugin_packaging folder

--- plugin_packaging/build_plugin.sh
+++ plugin_packaging/build_plugin.sh
@@ -2,13 +2,11 @@
 
 current=`pwd`
 mkdir -p /tmp/actionSHARK/
-cp -R ./actionshark /tmp/actionSHARK/
-cp ./plugin_packaging/* /tmp/actionSHARK/
-cp ./setup.py /tmp/actionSHARK/
-cp ./main.py /tmp/actionSHARK
-cp ./logger_config.json /tmp/actionSHARK
-cp ./README.md /tmp/actionSHARK/
-cp ./LICENSE /tmp/actionSHARK/
+cp * /tmp/actionSHARK/
+cp -R ../actionSHARK /tmp/actionSHARK/
+cp ../setup.py /tmp/actionSHARK/
+cp ../main.py /tmp/actionSHARK/
+cp ../logger_config.json /tmp/actionSHARK/
 cd /tmp/actionSHARK/
 
 
@@ -16,4 +14,4 @@ if [ -f "$current/actionSHARK_plugin.tar" ]; then
     rm "$current/actionSHARK_plugin.tar"
 fi
 
-tar -cvf "$current/actionSHARK_plugin.tar" --exclude=*.tar --exclude=build_plugin.sh --exclude=*/tests --exclude=*/.env --exclude=*/.vscode --exclude=*/__pycache__ --exclude=*.pyc *
+tar -cvf "$current/actionSHARK_plugin.tar" --exclude=*.tar --exclude=build_plugin.sh --exclude=*/tests --exclude=*/__pycache__ --exclude=*.pyc *
--- plugin_packaging/install.sh
+++ plugin_packaging/install.sh
@@ -2,5 +2,4 @@
 PLUGIN_PATH=$1
 cd $PLUGIN_PATH
 
-# Install actionSHARK
 python $PLUGIN_PATH/setup.py install --user
